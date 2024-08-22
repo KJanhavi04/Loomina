ccae521a8e24bb7d21e4762b884ea5bad5421a19
NamitaWarang <namitawarang1@gmail.com>
2024-08-22 11:15:21 +0530

    Loomina_frontend Folder created. Basic Login and Sign Up page created.

---
+++ loomina_frontend/src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* Add other routes here as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
