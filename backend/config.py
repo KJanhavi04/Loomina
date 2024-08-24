6251903d72bcd095a7d97538b62b1946b777d935
shru2004 <shrutipatil10951@gmail.com>
2024-08-24 16:59:11 +0530

    Login and Signup works

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = "d2b1d6a4b9da4721ae111b331d481168"
     JWT_SECRET_KEY = '4f8f85bae19b40d69ba3fd5d593b6b93'
+    SECRET_KEY = 'd2b1d6a4b9da4721ae111b331d481168'
+    MONGO_URI = 'mongodb://localhost:27017/Loomina'
     DEBUG = True
