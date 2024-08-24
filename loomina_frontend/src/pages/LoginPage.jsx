64a9c158fb8a72ad0200bacd655cdc264a16b0dc
KJanhavi04 <k.janhavi04@gmail.com>
2024-08-24 18:37:16 +0530

    CreateSpark page created (Basic Structure) with add more and cancel features, thread conncetor created but it needs to be polished, overall a basic page is created.
    CreateSpark.jsx
    create.css
    
    Different folders for create and master - (header, footer, sidebar, master!)

--- loomina_frontend/src/pages/LoginPage.jsx
+++ loomina_frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-
+ 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
--- loomina_frontend/src/pages/Master/DashboardPage.jsx
+++ loomina_frontend/src/pages/Master/DashboardPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import MasterPage from '../../components/master/Master';
+import CreateSpark from '../../components/create/CreateSpark';
 
 const DashboardPage = () => {
   return (
     <MasterPage>
-      <div className="text-center h-full flex items-center justify-center">
+      <div>
         {/* Placeholder for main content */}
-        <div className="text-3xl text-gray-400">Dashboard Content</div>
+        {/* <div className="text-3xl text-gray-400">Dashboard Content</div> */}
+        <CreateSpark>
+
+        </CreateSpark>
       </div>
     </MasterPage>
   );
